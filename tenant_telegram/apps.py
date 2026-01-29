6aca9bd3b7ca77f41ea163afba369f5e8b58bf5b
Prem Megha <premmegha91@gmail.com>
2026-01-30 01:45:15 +0530

    Reset repo to empty state

--- tenant_telegram/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TenantTelegramConfig(AppConfig):
-    name = 'tenant_telegram'
--- tenant_telegram/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import telegram_settings_view
-
-urlpatterns = [
-    path("settings/", telegram_settings_view, name="telegram_settings"),
-]
